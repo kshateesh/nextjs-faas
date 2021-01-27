3752410af32c32fd8b1c1e74bb61b9ce2fc1386a
kshateesh <kshateesh007@gmail.com>
2021-01-27 18:41:56 +0530

    add logs

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const si = require("systeminformation");
 
 export default (req, res) => {
+  si.osInfo()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
+  si.currentLoad()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
+  si.processes()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
+  si.services("*")
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
+  si.dockerInfo()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
   console.log("Test page app");
   res.statusCode = 200;
   res.json({ name: "John Doe" });
